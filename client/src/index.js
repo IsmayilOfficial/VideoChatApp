0fa81b86231627e5a7b487afe3b52605cd40ec87
Ismayil Tahmazov <tahmazov.ismayil@gmail.com>
2021-04-03 03:42:52 +0400

    frontend

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {ContextProvider} from './SocketContext';
+
+
+import App from './App';
+
+import './styles.css';
+
+ReactDom.render(
+    <ContextProvider>
+        <App />,
+    </ContextProvider>
+
+,
+document.getElementById('root')
+);
+
