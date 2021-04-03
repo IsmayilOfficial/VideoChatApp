6b34828793d2d2cc03b28094cc775845d9525195
Ismayil Tahmazov <tahmazov.ismayil@gmail.com>
2021-04-03 04:25:00 +0400

    Update SocketContext.js

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,8 +4,10 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-// const socket = io('http://localhost:5000');
-const socket = io('https://warm-wildwood-81069.herokuapp.com');
+ const socket = io('http://localhost:5000');
+
+// online server on heroku
+//const socket = io('https://fm-live.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
